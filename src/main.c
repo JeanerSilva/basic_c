36a79c765cbb591a0134a726e1fd2155a4dd728a
Jeaner Silva <jeanerufmg@gmail.com>
2026-01-16 10:20:33 -0300

    sec

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
-#include <stdio.h>    // Necessário para o printf 
-#include "coord.h"    // Necessário para o tipo coordinate_t e new_coord [cite: 1, 2]
+#include <stdio.h>    
+#include "coord.h"    
 
 int main() {
-    // 1. Criamos a "instância"
     coordinate_t c = new_coord(1, 2, 3);
     
-    // 2. Chamamos o "método" através do ponteiro de função
+    // Chamamos o "método" através do ponteiro de função
     // Precisamos passar &c porque em C o 'self/this' não é automático
     coordinate_t c_scaled = c.scale(&c, 2);
 
